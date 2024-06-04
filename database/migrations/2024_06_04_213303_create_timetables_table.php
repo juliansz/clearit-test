551afa4d1dc032fbe767e8631b5aaeb97ade2505
Julian <juliansznaider@thenextweb.com>
2024-06-04 18:43:46 -0300

    create migrations for the tables (classrooms, timetables and classes)

---
+++ database/migrations/2024_06_04_213303_create_timetables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->id();
+            $table->integer('day_of_week');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->unsignedBigInteger('classroom_id')->nullable();
+            $table->foreign('classroom_id')->references('id')->on('classrooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timetables');
+    }
+};
